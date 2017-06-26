2c4bd8e63c7d61c2e158f05fe8615bb664b3fb63
Ovadia Shalom <ovidubs@gmail.com>
2017-06-25 20:45:08 -0500

    Update table6.php

--- table6.php
+++ table6.php
@@ -9,12 +9,12 @@ $con = @mysql_connect("localhost", "root", "");
 if(!$con) {
 	die("Cannot connect: " . mysql_error());
 }
-echo "<center><h1>Car Database for SER 322</h1></center> <hr />";
+echo "<center><h1>Automotive Controller Database for SER 322</h1></center> <hr />";
 mysql_select_db("Cars", $con);
 
 //update
 if (isset($_POST['update'])) {
-	$UpdateQuery = "UPDATE `controller` SET `ControllerID`='$_POST[controllerid]', Name='$_POST[name]', `Supplier`='$_POST[supplier]' WHERE `ControllerID`='$_POST[hidden]'";  
+	$UpdateQuery = "UPDATE `controller` SET `ControllerID`='$_POST[controllerid]', CarID='$_POST[carid]' ,Name='$_POST[name]', `Supplier`='$_POST[supplier]' WHERE `ControllerID`='$_POST[hidden]'";  
     mysql_query($UpdateQuery, $con);
 };
 
@@ -26,7 +26,7 @@ if (isset($_POST['delete'])) {
 
 //add
 if (isset($_POST['add'])) {
-	$AddQuery = "INSERT INTO `controller` (`ControllerID`, Name, `Supplier`) VALUES ('$_POST[ucontrollerid]', '$_POST[uname]', '$_POST[usupplier]')";
+	$AddQuery = "INSERT INTO `controller` (`ControllerID`, CarID, Name, `Supplier`) VALUES ('$_POST[ucontrollerid]', '$_POST[ucarid]', '$_POST[uname]', '$_POST[usupplier]')";
     mysql_query($AddQuery, $con);
 };
 
@@ -37,6 +37,7 @@ echo "<h3>Controller Table</h3>";
 echo "<table border=1>
 <tr>
 <th>ControllerID</th>
+<th>CarID</th>
 <th>Name</th>
 <th>Supplier</th>
 </tr>";
@@ -44,6 +45,7 @@ while($record = mysql_fetch_array($myData)) {
 	echo "<form action=table6.php method=post>";
 	echo "<tr>";
 	echo "<td>" . "<input type=text name=controllerid value=" . $record['ControllerID'] . " </td>";
+	echo "<td>" . "<input type=text name=carid value=" . $record['CarID'] . " </td>";
 	echo "<td>" . "<input type=text name=name value=" . $record['Name'] . " </td>"; 
 	echo "<td>" . "<input type=text name=supplier value=" . $record['Supplier'] . " </td>";
 	echo "<td>" . "<input type=hidden name=hidden value=" . $record['ControllerID'] . " </td>";
@@ -55,6 +57,7 @@ while($record = mysql_fetch_array($myData)) {
 echo "<form action=table6.php method=post>";
 echo "<tr>";
 echo "<td><input type=text name=ucontrollerid></td>";
+echo "<td><input type=text name=ucarid></td>";
 echo "<td><input type=text name=uname></td>";
 echo "<td><input type=text name=usupplier></td>";
 echo "<td>" . "<input type=submit name=add value=add" . " </td>";
