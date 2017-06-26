7fda25105abecbb558ee425d9d633ff574987442
Ovadia Shalom <ovidubs@gmail.com>
2017-06-25 20:43:33 -0500

    Update table1.php

--- table1.php
+++ table1.php
@@ -9,12 +9,12 @@ $con = @mysql_connect("localhost", "root", "");
 if(!$con) {
 	die("Cannot connect: " . mysql_error());
 }
-echo "<center><h1>Car Database for SER 322</h1></center> <hr />";
+echo "<center><h1>Automotive Controller Database for SER 322</h1></center> <hr />";
 mysql_select_db("Cars", $con);
 
 //update
 if (isset($_POST['update'])) {
-	$UpdateQuery = "UPDATE car SET CarID='$_POST[carid]', Model='$_POST[model]', Version='$_POST[version]', Type='$_POST[type]' WHERE CarID='$_POST[hidden]'";  
+	$UpdateQuery = "UPDATE car SET CarID='$_POST[carid]', Make='$_POST[make]', Model='$_POST[model]', Type='$_POST[type]', Year='$_POST[year]'  WHERE CarID='$_POST[hidden]'";  
     mysql_query($UpdateQuery, $con);
 };
 
@@ -26,7 +26,7 @@ if (isset($_POST['delete'])) {
 
 //add
 if (isset($_POST['add'])) {
-	$AddQuery = "INSERT INTO car (CarID, Model, Version, Type) VALUES ('$_POST[ucarid]', '$_POST[umodel]', '$_POST[uversion]', '$_POST[utype]')";
+	$AddQuery = "INSERT INTO car (CarID, Make, Model, Type, Year) VALUES ('$_POST[ucarid]', '$_POST[umake]', '$_POST[umodel]', '$_POST[utype]', '$_POST[uyear]')";
     mysql_query($AddQuery, $con);
 };
 
@@ -37,17 +37,19 @@ echo "<h3>Car Table</h3>";
 echo "<table border=1>
 <tr>
 <th>CarID</th>
+<th>Make</th>
 <th>Model</th>
-<th>Version</th>
 <th>Type</th>
+<th>Year</th>
 </tr>";
 while($record = mysql_fetch_array($myData)) {
 	echo "<form action=table1.php method=post>";
 	echo "<tr>";
 	echo "<td>" . "<input type=text name=carid value=" . $record['CarID'] . " </td>";
-	echo "<td>" . "<input type=text name=model value=" . $record['Model'] . " </td>"; 
-	echo "<td>" . "<input type=text name=version value=" . $record['Version'] . " </td>";
+	echo "<td>" . "<input type=text name=make value=" . $record['Make'] . " </td>"; 
+	echo "<td>" . "<input type=text name=model value=" . $record['Model'] . " </td>";
 	echo "<td>" . "<input type=text name=type value=" . $record['Type'] . " </td>";
+	echo "<td>" . "<input type=text name=year value=" . $record['Year'] . " </td>";
 	echo "<td>" . "<input type=hidden name=hidden value=" . $record['CarID'] . " </td>";
 	echo "<td>" . "<input type=submit name=update value=update" . " </td>";
 	echo "<td>" . "<input type=submit name=delete value=delete" . " </td>";
@@ -57,9 +59,10 @@ while($record = mysql_fetch_array($myData)) {
 echo "<form action=table1.php method=post>";
 echo "<tr>";
 echo "<td><input type=text name=ucarid></td>";
+echo "<td><input type=text name=umake></td>";
 echo "<td><input type=text name=umodel></td>";
-echo "<td><input type=text name=uversion></td>";
 echo "<td><input type=text name=utype></td>";
+echo "<td><input type=text name=uyear></td>";
 echo "<td>" . "<input type=submit name=add value=add" . " </td>";
 echo "</form>";
 echo "</table>";
